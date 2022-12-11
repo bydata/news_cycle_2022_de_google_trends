934ed479f63f8ff1bf5fbb926a30d73fc56b279a
bydata <ansgar.wolsing@gmail.com>
2022-12-11 10:34:27 +0100

    vis 2022

--- R/vis_2022.R
+++ R/vis_2022.R
@@ -11,19 +11,14 @@ google_colors <- c("#4285F4", "#DB4437", "#F4B400", "#0F9D58")
 google_colors_desaturated <- colorspace::desaturate(google_colors, 0.2)
 
 
-
 ## DATA PREP ===================================================================
 
-trends_combined <- read_rds(file.path("data", "trends_2021_combined.rds"))
+trends_combined <- read_rds(file.path("data", "trends_2022_combined.rds"))
 
 # keywords
 keywords <- unique(trends_combined$keyword)
 # exclude some keywords manually
-keywords <- keywords[!keywords %in% c("WhatsApp", "Biontech", "Curevac", "2G+",
-                                      "Markus Söder", "PCR-Test", "Farbfilm",
-                                      "Kuba-Syndrom", "Bärbel Bas", "Gil Ofarim",
-                                      "Christian Eriksen", "Karl Lauterbach",
-                                      "Wetten dass", "Alec Baldwin")]
+keywords <- keywords[!keywords %in% c()]
 # sort keywords alphabetically
 keywords <- keywords[order(keywords)]
 
@@ -48,7 +43,7 @@ keywords_by_peak_date <- trends_combined %>%
     # add a colored dot as a guidance in the y-axis labels
     label = glue::glue(
       "{keyword}
-      <span style='color: {row_color};font-family:sans-serif; font-size:12pt'>\u2022</span>")
+      <span style='color: {row_color};font-family:Arial;'>\u2022</span>")
   ) %>%
   select(-row_color)
 
@@ -74,7 +69,7 @@ df_plot <- trends_combined %>%
 
 # Annotations
 plot_titles <- list(
-  title = glue("Google Trends <span style='color:{google_colors[1]}'>2021</span>"),
+  title = glue("Google Trends <span style='color:{google_colors[1]}'>2022</span>"),
   subtitle = "Tägliches relatives Suchaufkommen ausgewählter Suchbegriffe Deutschland",
   caption = glue::glue("Rollierender Mittelwert (7 Tage, zentriert) des 
   täglichen Suchaufkommens skaliert auf einen Wertbereich von 
@@ -86,7 +81,7 @@ g <- df_plot %>%
   ggplot(aes(date, y, group = keyword)) +
   geom_line(aes(fill = factor(row %% length(google_colors))), # for colouring the areas later 
             col = "white", 
-            size = 0.2) + 
+            linewidth = 0.2) + 
   scale_x_datetime(
     expand = c(0.0005, 0), 
     date_breaks = "1 month", date_labels = "%b", position = "top") +
@@ -137,38 +132,15 @@ gg_df <- data.frame(x = gg$data[[1]]$x,
 # add the ribbon with smoothed values as ymax and keyword height as ymin
 p2 <- g + geom_ribbon(data = gg_df,
                 aes(x = as_datetime(x), ymin = added, ymax = ymax, group = group, fill = fill), 
-                inherit.aes = FALSE, alpha = 0.7) +
+                inherit.aes = FALSE, alpha = 0.7, col = "white", size = 0.2) +
   scale_fill_identity()
 
-ggsave(file.path("plots", "gtrends_de_2021_dark-smoothed-7d.png"), device = ragg::agg_png,
+ggsave(file.path("plots", "gtrends_de_2022_dark-smoothed-7d.png"), device = ragg::agg_png,
        dpi = 400, width = 5.25, height = 8)
 
-# ggsave(file.path("plots", "gtrends_de_2021_dark.png"), device = ragg::agg_png,
-#        dpi = 400, width = 5.25, height = 8)
-# ggsave(file.path("plots", "gtrends_de_2021_dark-smoothed-5d.png"), device = ragg::agg_png,
-#        dpi = 400, width = 5.25, height = 8)
 
 
-
-# library(patchwork)
-
-p2 + labs(title = NULL, subtitle = "", 
-          tag = glue("GOOGLE TRENDS <span style='color:{google_colors[1]}'>2021</span>")
-          ) +
-  theme(
-    plot.tag = element_markdown(
-      family = "Roboto Condensed", face = "bold",
-      margin = margin(t = 6, l = 20, r = 0, b = 8),
-      size = 36, color = "white",
-      angle = 270, hjust = 0),
-    plot.margin = margin(r = 60),
-    plot.tag.position = c(1.05, 0.93))
-
-ggsave(file.path("plots", "gtrends_de_2021_dark-smoothed-7d_title_right.png"), device = ragg::agg_png,
-       dpi = 400, width = 6, height = 7.5)
-
-
-## Light version (needs polishing) ---------------------------------------------
+## Light version ---------------------------------------------
 
 p2 + theme(
   plot.background = element_rect(color = NA, fill = "white"),
@@ -183,43 +155,6 @@ p2 + theme(
                                         margin = margin(t = 16, b = 6))
 )
 
-ggsave(file.path("plots", "gtrends_de_2021_light-smoothed-7d.png"), device = ragg::agg_png,
+ggsave(file.path("plots", "gtrends_de_2022_light-smoothed-7d.png"), device = ragg::agg_png,
        dpi = 400, width = 6, height = 7.5)
 
-## Animation (needs polishing) ---------------------------------------------
-
-library(gganimate)
-
-## Use geom_area instead
-p3 <- df_plot %>%
-  ggplot(aes(date, y, group = keyword)) +
-  geom_ribbon(aes(ymin = added, ymax = y, fill = factor(row %% length(google_colors))), # for colouring the areas later
-            col = "white", size = 0.2, alpha = 0.5, show.legend = FALSE) +
-  scale_x_datetime(expand = c(0, 0), date_breaks = "1 month", date_labels = "%b", position = "top") +
-  scale_y_continuous(breaks = seq(added_height, added_height * length(keywords), added_height),
-                     labels = keywords_by_peak_date$keyword, expand = c(0, 0)) +
-  scale_fill_manual(values = google_colors) +
-  labs(title = plot_titles$title,
-       subtitle = plot_titles$subtitle,
-       caption = plot_titles$caption,
-       x = NULL, y = NULL) +
-  theme_minimal(base_family = "Raleway") +
-  theme(plot.title = element_markdown(family = "Source Sans Pro SemiBold",
-                                      margin = margin(t = 6, l = 6, r = 6, b = 10),
-                                      size = 18,
-                                      color = "white"),
-        plot.subtitle = element_textbox_simple(margin = margin(l = 6, b = 12), color = "grey95",
-                                               lineheight = 1.2),
-        plot.caption = element_textbox_simple(color = "grey95", size = 8, hjust = 0,
-                                              margin = margin(t = 16, b = 6),
-                                              lineheight = 1.1),
-        plot.title.position = "plot",
-        plot.margin = margin(t = 4, l = 12, r = 12, b = 4),
-        axis.text.y = element_markdown(hjust = 1, vjust = 0.5, color = "grey95",
-                                   margin = margin(l = 6, r = 4)),
-        axis.text.x = element_text(hjust = 0, color = "grey95"),
-        plot.background = element_rect(color = NA, fill = "grey8"),
-        panel.grid.major = element_line(size = 0.1, color = "grey40"),
-        panel.grid.minor = element_blank())
-
-p3 + transition_reveal(date)
